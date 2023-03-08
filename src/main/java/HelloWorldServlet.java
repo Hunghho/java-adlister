6db35f967c5dd5426632d2198aa5cd87e1280d09
Hung Ho <hung.ho902@gmail.com>
2023-03-08 16:49:51 -0600

    feat: servlet helloworld

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,16 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+// User goes to http://localhost:8080/hello in the browser and sees "hello world"
+@WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<h1 style='color:red'>Hello World</h1><a href = '/login'>login</a><script>alert('hello')</script>");
     }
 }
